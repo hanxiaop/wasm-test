8d295f48f1b532a35230dc217eda72f93d099ec9
xiaopeng <hanxiaop8@outlook.com>
2022-06-22 13:52:47 +0800

    add

--- main.go
+++ main.go
@@ -10,88 +10,84 @@ func main() {
 }
 
 type vmContext struct {
-	// Embed the default VM context here,
-	// so that we don't need to reimplement all the methods.
 	types.DefaultVMContext
 }
 
-// Override types.DefaultVMContext.
 func (*vmContext) NewPluginContext(contextID uint32) types.PluginContext {
-	return &pluginContext{counter: proxywasm.DefineCounterMetric("proxy_wasm_go.connection_counter")}
+	return &pluginContext{}
 }
 
 type pluginContext struct {
-	// Embed the default plugin context here,
-	// so that we don't need to reimplement all the methods.
 	types.DefaultPluginContext
-	counter proxywasm.MetricCounter
 }
 
-// Override types.DefaultPluginContext.
 func (ctx *pluginContext) NewTcpContext(contextID uint32) types.TcpContext {
-	return &networkContext{counter: ctx.counter}
+	return &networkContext{}
 }
 
 type networkContext struct {
-	// Embed the default tcp context here,
-	// so that we don't need to reimplement all the methods.
 	types.DefaultTcpContext
-	counter proxywasm.MetricCounter
 }
 
-// Override types.DefaultTcpContext.
-func (ctx *networkContext) OnNewConnection() types.Action {
-	proxywasm.LogInfo("new connection!")
-	return types.ActionContinue
-}
-
-// Override types.DefaultTcpContext.
 func (ctx *networkContext) OnDownstreamData(dataSize int, endOfStream bool) types.Action {
-	if dataSize == 0 {
+	addr, err := proxywasm.GetProperty([]string{"source", "address"})
+	if err != nil {
+		proxywasm.LogCriticalf("failed to get request data: %v", err)
 		return types.ActionContinue
 	}
+	proxywasm.LogCriticalf("source host address: %s", string(addr))
+
+	port, err := proxywasm.GetProperty([]string{"source", "port"})
+	if err != nil {
+		proxywasm.LogCriticalf("failed to get request data: %v", err)
+		return types.ActionContinue
+	}
+	proxywasm.LogCriticalf("source host port: %s", string(port))
+
+	daddr, err := proxywasm.GetProperty([]string{"destination", "address"})
+	if err != nil {
+		proxywasm.LogCriticalf("failed to get request data: %v", err)
+		return types.ActionContinue
+	}
+	proxywasm.LogCriticalf("destination address: %s", string(daddr))
+
+	dport, err := proxywasm.GetProperty([]string{"destination", "port"})
+	if err != nil {
+		proxywasm.LogCriticalf("failed to get request data: %v", err)
+		return types.ActionContinue
+	}
+	proxywasm.LogCriticalf("destination port: %s", string(dport))
+
+	ulocal, err := proxywasm.GetProperty([]string{"upstream", "local_address"})
+	if err != nil {
+		proxywasm.LogCriticalf("failed to get request data: %v", err)
+		return types.ActionContinue
+	}
+	proxywasm.LogCriticalf("upstream local address: %s", string(ulocal))
 
 	data, err := proxywasm.GetDownstreamData(0, dataSize)
+	proxywasm.AppendDownstreamData([]byte{})
 	if err != nil && err != types.ErrorStatusNotFound {
 		proxywasm.LogCriticalf("failed to get downstream data: %v", err)
 		return types.ActionContinue
 	}
-
-	proxywasm.LogInfof(">>>>>> downstream data received >>>>>>\n%s", string(data))
+	proxywasm.LogCriticalf("downstream data, size %d, data: %s", dataSize, string(data))
 	return types.ActionContinue
 }
 
-// Override types.DefaultTcpContext.
-func (ctx *networkContext) OnDownstreamClose(types.PeerType) {
-	proxywasm.LogInfo("downstream connection close!")
-	return
-}
-
-// Override types.DefaultTcpContext.
 func (ctx *networkContext) OnUpstreamData(dataSize int, endOfStream bool) types.Action {
-	if dataSize == 0 {
-		return types.ActionContinue
-	}
-
 	ret, err := proxywasm.GetProperty([]string{"upstream", "address"})
 	if err != nil {
 		proxywasm.LogCriticalf("failed to get upstream data: %v", err)
 		return types.ActionContinue
 	}
 
-	proxywasm.LogInfof("remote address: %s", string(ret))
+	proxywasm.LogCriticalf("remote address: %s", string(ret))
 
 	data, err := proxywasm.GetUpstreamData(0, dataSize)
 	if err != nil && err != types.ErrorStatusNotFound {
 		proxywasm.LogCritical(err.Error())
 	}
-
-	proxywasm.LogInfof("<<<<<< upstream data received <<<<<<\n%s", string(data))
+	proxywasm.LogCriticalf("upstream data, size %d, data: %s", dataSize, string(data))
 	return types.ActionContinue
-}
-
-// Override types.DefaultTcpContext.
-func (ctx *networkContext) OnStreamDone() {
-	ctx.counter.Increment(1)
-	proxywasm.LogInfo("connection complete!")
 }
